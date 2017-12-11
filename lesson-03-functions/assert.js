8b3f98c1b377d080c9c0b39402ddab8f6301aeb6
sonnb1812 <sonnb@vnext.vn>
2017-12-11 14:22:18 +0700

    refactor

--- lesson-03-functions/assert.js
+++ lesson-03-functions/assert.js
@@ -3,12 +3,8 @@ function assertEquals(actual, expected, message ) {
   const FAILED = "FAILED"
 
   let result = expected === actual
-  logResult(result, message)
-
-  function logResult (res, mes) {
-    let log = res ? PASS : FAILED
-    log += ": " +  mes
-    console.log(log)
-  }
+  let prefix = result ? PASS : FAILED
+  let info = prefix + ": " +  message
+  console.log(info)
 
 }
