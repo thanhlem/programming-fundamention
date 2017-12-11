c96f34115e064232c010318a4c5bdeecd572884c
sonnb1812 <sonnb@vnext.vn>
2017-12-11 10:50:38 +0700

    simple assert library

---
+++ lesson-03-functions/assert.js
@@ -0,0 +1,14 @@
+function assertEqual( actual, expected, message ) {
+  const PASS = "PASS"
+  const FAILED = "FAILED"
+
+  let result = expected === actual
+  logResult(result, message)
+
+  function logResult (res, mes) {
+    let log = res ? PASS : FAILED
+    log += ": " +  mes
+    console.log(log)
+  }
+
+}
