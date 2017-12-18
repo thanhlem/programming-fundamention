cf29be6a20650d396f2ab77fe5de6ad041b59c1f
THANHLEM <thanhlm.rsc@gmail.com>
2017-12-18 14:51:23 +0700

    test commmit function

--- lesson-05-looping/rectangle.js
+++ lesson-05-looping/rectangle.js
@@ -1,3 +1,13 @@
 'use strict'
 
 // write drawRectangle function here
+function drawRectangle(x, y) {
+
+    if (x === 1) {
+        return '*\n';
+    } else {
+        return '************\n';
+    }
+
+
+}
