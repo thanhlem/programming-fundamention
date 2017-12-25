0ec5ec5f07149e5f32df7844b45bf38a7f8894bc
binhsonnguyen <binhson.nguyen@icloud.com>
2017-12-25 14:02:18 +0700

    linesegment library

--- libs/line-segment.js
+++ libs/line-segment.js
@@ -1,1 +1,25 @@
 'use strict'
+
+function createLineSegment (len, end, line) {
+  if (!len || isNaN(len) || len < 0) return
+
+  return {
+    length: len || 0,
+    end: end || '*',
+    line: line || '*',
+    print: function () {
+      let visualized = ''
+      let remaining = this.length
+      while (remaining > 0) {
+        let isEnd = remaining === this.length || remaining === 1
+        if (isEnd) {
+          visualized += this.end
+        } else {
+          visualized += this.line
+        }
+        remaining--
+      }
+      return visualized
+    }
+  }
+}
